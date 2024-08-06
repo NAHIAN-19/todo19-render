e3931a618a4253f6744f402f480256bf64594759
SILVER KING <nahiannazeeb19@gmail.com>
2024-08-06 23:00:03 +0600

    todo-19 render more changes - 2

--- Todo_List/settings.py
+++ Todo_List/settings.py
@@ -100,7 +100,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
